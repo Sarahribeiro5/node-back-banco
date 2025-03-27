fd943fbfb1ad75ef9184a746aea3138511f8657b
Sarahribeiro5 <158165830+Sarahribeiro5@users.noreply.github.com>
2025-03-27 14:28:06 -0300

    feat: first commit

--- src/models/tarefaModel.js
+++ src/models/tarefaModel.js
@@ -1,46 +1,55 @@
-import prisma from '../../prisma/client.js';
-
-class TarefaModel {
-  getAll = async () => {
-    return await prisma.task.findMany();
-  };
-
-  create = async (descricao) => {
-    return await prisma.task.create({
-      data: {
-        descricao,
-      }, 
-    })
-  };
-
-  update = async (id, concluida, descricao) => {
-    try {
-      const tarefa = await prisma.task.update({
-        where: { id },
-        data: {
-          concluida: concluida !== undefined ? concluida : true,
-          descricao,
-        },
-      });
-
-      return tarefa;
-    } catch (error) {
-      console.log("Error", error);
-      throw error;
-    }
-  };
-   
-  delete = async (id) => {
-    try {
-      const tarefaDeletada = await prisma.task.delete({
-        where: { id },
-      });
-
-      return tarefaDeletada;
-    } catch (error) {
-      console.log("Erro ao deletar a tarefa!", error);
-      throw error;
-    }
-  };
-}
-export default new TarefaModel();
+import prisma from "../../prisma/client.js";
+ 
+ class TarefaModel {
+   getAll = async () => {
+     return await prisma.task.findMany();
+   };
+ 
+   create = async (descricao) => {
+     return await prisma.task.create({
+       data: {
+         descricao,
+       },
+     });
+   };
+ 
+   update = async (id, concluida) => {
+     try {
+       return await prisma.task.update({
+         where: { id },
+         data: {
+           concluida: concluida !== undefined ? concluida : true,
+         },
+       });
+     } catch (error) {
+       // Se a tarefa não for encontrada, o Prisma lançará uma exceção
+       if (error.code === "P2025") {
+         return null;
+       }
+       throw error;
+     }
+   };
+ 
+   delete = async (id) => {
+     try {
+       await prisma.task.delete({
+         where: { id },
+       });
+       return true;
+     } catch (error) {
+       // Se a tarefa não for encontrada, o Prisma lançará uma exceção
+       if (error.code === "P2025") {
+         return false;
+       }
+       throw error;
+     }
+   };
+ 
+   getById = async (id) => {
+     return await prisma.task.findUnique({
+       where: { id },
+     });
+   };
+ }
+ 
+ export default new TarefaModel();
